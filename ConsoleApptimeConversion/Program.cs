354b46d5b396e595f1f54a7a5dcda9bf86634645
Marlon Santos <marlonssantos@yahoo.com>
2019-05-23 00:12:42 -0500

    Added Explanation

--- ConsoleApptimeConversion/Program.cs
+++ ConsoleApptimeConversion/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApptimeConversion
     {
         static void Main(string[] args)
         {
+            // Given a time in -hour AM / PM format, convert it to military(24 - hour) time.
+            // Note: Midnight is 12:00:00AM on a 12 - hour clock, and 00:00:00 on a 24 - hour clock.Noon is 12:00:00PM on a 12 - hour clock, and 12:00:00 on a 24 - hour clock.
+            // Function Description
+            // Complete the timeConversion function in the editor below.It should return a new string representing the input time in 24 hour format.
+            
             string s = Console.ReadLine();
 
             string result = Result.TimeConversion(s);
